103003680fa8c64b25f7682003151a729988db0c
Thedarkness-bd <toisiftasrik@gmail.com>
2023-12-14 18:55:58 +0600

    Sample problem 1 | Sol | added

---
+++ SWITCH CODE/Sample1.c
@@ -0,0 +1,52 @@
+// Create a program that takes a number representing a month (1 for January, 2 for February, etc.) and outputs the name of the month.
+
+#include <stdio.h>
+int main()
+{
+    int month_number;
+    printf("Enter the month number: ");
+    scanf("%d", &month_number);
+    switch (month_number)
+    {
+    case 1:
+        printf("January\n");
+        break;
+    case 2:
+        printf("February\n");
+        break;
+    case 3:
+        printf("March\n");
+        break;
+    case 4:
+        printf("April\n");
+        break;
+    case 5:
+        printf("May\n");
+        break;
+    case 6:
+        printf("June\n");
+        break;
+    case 7:
+        printf("July\n");
+        break;
+    case 8:
+        printf("August\n");
+        break;
+    case 9:
+        printf("September\n");
+        break;
+    case 10:
+        printf("October\n");
+        break;
+    case 11:
+        printf("November\n");
+        break;
+    case 12:
+        printf("December\n");
+        break;
+    default:
+        printf("Invalid Month number\n");
+        break;
+    }
+    return 0;
+}
