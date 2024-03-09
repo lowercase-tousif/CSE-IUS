3bfe589d798f10fe1c942403019dd4c98ee1891a
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-10 00:02:00 +0600

    14 fixed

--- ius-suggestion/14.c
+++ ius-suggestion/14.c
@@ -5,15 +5,14 @@ int main()
     char str[50];
     gets(str);
 
+    printf("Vowels Found: ");
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
         {
-            c++;
+            printf("%c", str[i]);
         }
     }
-
-    printf("There are %d vowels in this string %s.\n", c, str);
 
     return 0;
 }
